6b5a9e80b6cb6f1ab7ef85f1732f6ecf34502cba
Brent Knudsen <brent@Brents-MacBook-Pro.local>
2021-02-26 09:07:44 -0700

    html structure, layout, css framework, font and color styles

---
+++ main.js
@@ -0,0 +1,13 @@
+bids = [];
+
+function placeBid() {
+
+}
+
+// Create an element that will display an array of bids
+// Create two input boxes and two “Place Bid” buttons
+// Create a function that will add the array of bids to the element (1)
+// Create a function that will take the text in an input box and add it to the array of bids
+// Add the function above to the click event of the “Place Bid” buttons 
+// Create a function that will save the array of bids to localStorage
+// Create a function that will retrieve the array of bids from localStorage
